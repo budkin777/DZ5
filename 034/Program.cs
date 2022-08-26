7dace056491f0c4c21ef6175697078d411aac56e
Aleksandr <budkin86bk@yandex.ru>
2022-08-26 17:17:29 +0500

    Практическое задание к семинару 5

---
+++ 034/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = new int[10];
+int arraySize = array.Length;
+int count = 0;
+
+for (int i = 0; i < arraySize; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if (array[i] % 2 == 0) count++;
+}
+Console.WriteLine($"В массиве [{String.Join(", ", array)}] {count} чётных чисел");
+
+
---
+++ 038/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] arr = new double[10];
+int arrSize = arr.Length;
+
+for (int i = 0; i < arrSize; i++)
+{
+    arr[i] = new Random().NextDouble();
+    arr[i] = arr[i] * 100;
+}
+double maxNumber = arr[0];
+double minNumber = arr[0];
+
+for(int j = 0; j < arrSize; j++)
+{
+    if(maxNumber < arr[j]) maxNumber = arr[j];
+    if(minNumber > arr[j]) minNumber = arr[j];
+}
+double difference = maxNumber - minNumber;
+Console.Write($"Массив [{String.Join(", ", arr)}] \n");
+Console.Write($"Разница между {maxNumber} и {minNumber} = {difference}");
+
+
